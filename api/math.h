b341ea73b3a7566806d79747ce14dc50c897ea32
getsadzeg <getsadzeguram@gmail.com>
2016-01-06 18:23:44 +0400

    delete math header and source file

--- api/math.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATH_H
-#define MATH_H
-void sum(int x,int y);
-void subtraction(int x,int y);
-void divide(int x,int y);
-void multiply(int x,int y);
-void fromto(int n);
-void digitsnumb(int x, int y);
-#endif //MATH_H
